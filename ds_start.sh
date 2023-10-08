4067695bbf76c1309d4d711ea4a1202a5895d1f9
xhy <841604950@qq.com>
2023-10-08 09:38:01 +0800

    updata

---
+++ ds_start.sh
@@ -0,0 +1,20 @@
+deepspeed --num_gpus 2 src/train_bash.py \
+    --deepspeed ds_config.json \
+    --stage sft \
+    --model_name_or_path  Qwen/Qwen-14B-Chat\
+    --do_train \
+    --dataset ros \
+    --template chatml \
+    --finetuning_type lora \
+    --lora_target c_attn \
+    --output_dir qwen_output \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 2000 \
+    --plot_loss \
+    --fp16
---
+++ start.sh
@@ -0,0 +1,21 @@
+CUDA_VISIBLE_DEVICES=0,1 python src/train_bash.py \
+    --use_auth_token true \
+    --hf_auth_token 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 \
+    --stage sft \
+    --model_name_or_path Qwen/Qwen-7B-Chat \
+    --do_train \
+    --dataset ros \
+    --template chatml \
+    --finetuning_type lora \
+    --lora_target c_attn \
+    --output_dir qwen_14b_output \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 100 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 1000 \
+    --plot_loss \
+    --fp16
